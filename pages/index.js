5db39a4d4073273684bc83754a8dd19644a547a8
Rick Hekman <rickhekman@DESKTOP-K3DOK10>
2022-10-19 15:17:46 +0200

    Add footer as component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Footer from '../components/Footer';
+
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
@@ -51,19 +52,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
