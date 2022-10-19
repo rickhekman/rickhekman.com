fbffa47142858d4b29cda52c08a28f6a62f40ff4
Rick Hekman <rickhekman@DESKTOP-K3DOK10>
2022-10-19 14:10:34 +0200

    Add sass as styles language

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const path = require('path')
+    
+    module.exports = {
+        sassOptions: {
+          includePaths: [path.join(__dirname, 'styles')],          
+        }
+    }
+
+// module.exports = (phase, {defaultConfig}) => {
+//   if ('sassOptions' in defaultConfig) {
+//       defaultConfig['sassOptions'] = {
+//           includePaths: ['/styles'],
+//           prependData: `@import "~@styles/main.scss";`,
+//       }
+//   }
+//   return defaultConfig;
+// }
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
