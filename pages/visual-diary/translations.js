096ee11a807b6735bee5630178b896eeb253efbd
Rick Hekman <info@rickhekman.com>
2022-12-16 16:23:48 +0100

    Add translations to visual diary page

---
+++ pages/visual-diary/translations.js
@@ -0,0 +1,17 @@
+export const en = {
+  visit: "Visit my",
+  insta: "Instagram profile",
+  more: "for more."
+}
+
+export const es = {
+  visit: "Visita mi",
+  insta: "perfil de Instagram",
+  more: "para más."
+}
+
+export const nl = {
+  visit: "Bezoek mijn",
+  insta: "Instagram profiel",
+  more: "voor meer."
+}
