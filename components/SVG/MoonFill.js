0f40c7219d66be305cbe255c344dcbfe9964c512
Rick Hekman <info@rickhekman.com>
2022-12-19 16:16:36 +0100

    Fix theme switch icons as sun and moon

---
+++ components/SVG/MoonFill.js
@@ -0,0 +1,7 @@
+export default function MoonFillIcon() {
+  return (
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="256" height="256" viewBox="0 0 256 256"><defs><clipPath><rect width="256" height="256"/></clipPath></defs><g><path d="M134,262q-53.333,0-90.667-37.333T6,134Q6,80.667,43.333,43.333T134,6q2.844,0,6.044.178t8.178.533A77.671,77.671,0,0,0,128.311,34.8,88.973,88.973,0,0,0,121.2,70q0,32,22.4,54.4T198,146.8a95.236,95.236,0,0,0,35.2-6.578,78.593,78.593,0,0,0,28.089-18.311q.356,4.267.533,6.933T262,134q0,53.333-37.333,90.667T134,262Z" transform="translate(-6 -6)" fill="#fff"/></g></svg>
+    </>
+  )
+};
