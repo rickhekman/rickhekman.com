46ed3898ff82cbca810c356c040acfbfa87511fc
Rick Hekman <info@rickhekman.com>
2022-12-20 16:25:01 +0100

    Add feature collapse menu item with submenu

--- components/Header/SideDrawer/index.js
+++ components/Header/SideDrawer/index.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import useTranslation from 'next-translate/useTranslation';
 
 import LocaleSwitcher from '../../LocaleSwitcher';
@@ -21,20 +22,29 @@ export default function SideDrawer(props) {
     tabindex = 0;
   }
 
+  const [isActive, setIsActive] = useState(false);
+
+
 
   return (
     <>
       <div className={drawerClasses}>
         <nav className={styles.nav}>
           <ul className={styles.menu}>
-            <li className={styles.item} onClick={props.click} >
+            <li onClick={props.click} >
               <Link href="/" tabIndex={tabindex}>Home</Link>
             </li>
-            <li className={styles.item} onClick={props.click}>
+            <li onClick={props.click}>
               <Link href="/projects" tabIndex={tabindex}>{t("menu:projects")}</Link>
             </li>
-            <li className={styles.item} onClick={props.click}>
-              <Link href="/visual-diary" tabIndex={tabindex}>{t("menu:visual")}</Link>
+            <li className={styles.item}>
+              <button type="button" aria-label={t("menu:open-photo-submenu-aria")} className={styles.button} onClick={() => setIsActive(!isActive)}>{t("menu:photography")} {isActive ? '-' : '+'} </button>
+                {isActive && <ul className={styles.submenu}>
+                    <li className={styles.subitem} onClick={props.click}>
+                      <Link href="/visual-diary" tabIndex={tabindex}>{t("menu:visual")}</Link>
+                    </li>
+                  </ul>
+                }
             </li>
           </ul>
           <div className={styles.locale}>
