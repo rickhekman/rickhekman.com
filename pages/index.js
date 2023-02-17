b107d66dda24d029b73b9d143d19895865460c8a
Rick Hekman <info@rickhekman.com>
2023-02-17 15:31:20 +0100

    Fix metadata component inside layout component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Head from 'next/head';
-
-import Metadata from '../components/Metadata';
 import Title from '../components/Title';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <Metadata />
-      </Head>
       <Title />
     </>
   )
